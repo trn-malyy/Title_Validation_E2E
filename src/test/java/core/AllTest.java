af8121c063d238338c1471baa8687e19a140ec57
Serhiy Malyy <serhiy.malyy@gmail.com>
2014-06-01 21:28:09 -0700

    commit

---
+++ src/test/java/core/AllTest.java
@@ -0,0 +1,12 @@
+package core;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+	@RunWith(Suite.class)
+	
+	@SuiteClasses({SeleniumTest.class})
+	public class AllTest {
+	
+}
